85b04093917b4cdf30817116a9104df317feb829
MSD-12 <magandra1999@gmail.com>
2024-02-01 23:58:01 +0530

    Day9

--- Day9_PlayingWithTextbox.java
+++ Day9_PlayingWithTextbox.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Day8_Locators1 {
+public class Day7_Locators1 {
     public static void main(String[] args) {
         // Set the path to your ChromeDriver executable
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
@@ -16,13 +16,17 @@ public class Day8_Locators1 {
         // Navigate to a website
         driver.get("https://www.github.com/login/");
 
-        // This will wait 2 secondsto perform next command
-        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(2));
-
-
         //Locators Exploration on Linkedin Login Page  id=""
         WebElement userName= driver.findElement(By.id("login_field"));
+        
+        //  typing username in textbox
+        userName.sendKeys("Github@test.com");
+
         WebElement password = driver.findElement(By.name("password"));
-        WebElement joinNow = driver.findElement(By.className("mt-1 mb-0 p-0"));
+        password.sendKeys("test@123.com");
+
+        // Clear password from textbox
+        driver.findElement(By.name("password")).clear();
+
     }
 }
